61be069f838f579cdcb1f0297c5e3fc3524351c1
Plotnikov V.D. <pltnikovqwerty@gmail.com>
2025-12-10 21:19:14 +0300

    DI-container extension

---
+++ FileAnalysisService/FileAnalysis.Infrastructure/DependencyInjection/InfrastructureServiceRegistration.cs
@@ -0,0 +1,43 @@
+using FileAnalysis.Application.Interfaces;
+using FileAnalysis.Domain.Services;
+using FileAnalysis.Infrastructure.Clients;
+using FileAnalysis.Infrastructure.Persistence;
+using FileAnalysis.Infrastructure.SimilarityAlgorithms;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FileAnalysis.Infrastructure.DependencyInjection;
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddFileAnalysisInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<FileAnalysisDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("FileStorageDb"));
+        });
+
+        services.AddScoped<IPlagiarismReportRepository, EfPlagiarismReportRepository>();
+        
+        services.AddHttpClient<IFileStorageClient, FileStorageClient>(client =>
+        {
+            client.BaseAddress = new Uri(
+                configuration["Services:FileStorage"] 
+                ?? throw new ArgumentNullException("FileStorageAddress")
+            );
+        });
+
+        services.AddScoped<ISimilarityAlgorithm>(sp =>
+        {
+            var pow = configuration.GetValue<double>(
+                "Similarity:Levenshtein:Power");
+
+            return new LevenshteinDistanceAlgorithm(pow);
+        });
+        
+        return services;
+    }
+}
